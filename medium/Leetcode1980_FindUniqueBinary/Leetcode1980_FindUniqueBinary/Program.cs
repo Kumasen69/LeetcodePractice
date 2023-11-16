e9e039ca1a954f392e86485f96d59750cbd9d8bb
Kumasen69 <kumasen69@gmail.com>
2023-11-16 21:08:02 +0800

    Runtime: 85ms(Beats 76%) Memory: 41.58MB(Beats 12%)

--- medium/Leetcode1980_FindUniqueBinary/Leetcode1980_FindUniqueBinary/Program.cs
+++ medium/Leetcode1980_FindUniqueBinary/Leetcode1980_FindUniqueBinary/Program.cs
@@ -6,10 +6,69 @@ using System.Threading.Tasks;
 
 namespace Leetcode1980_FindUniqueBinary
 {
+    public class Solution
+    {
+
+        public string DecToString(int input, int n)
+        {
+            string result = "";
+            while(input > 0)
+            {
+                result += (input % 2).ToString();
+                input /= 2;
+            }
+
+            while(result.Length < n)
+            {
+                result += '0';
+            }
+            return new string(result.Reverse().ToArray());
+        }
+
+        public int StringToDex(string input)
+        {
+            int result = 0;
+            int k = 1;
+
+            string inputRev = new string(input.Reverse().ToArray());
+
+            foreach(var i in inputRev)
+            {
+                result += k * int.Parse(i.ToString());
+                k *= 2;
+            }
+
+            return result;
+        }
+
+        public string FindDifferentBinaryString(string[] nums)
+        {
+            HashSet<int> hashInt = new HashSet<int>();
+
+            foreach(var i in nums)
+            {
+                hashInt.Add(StringToDex(i));
+            }
+
+            double range = Math.Pow(2, nums.Length);
+
+            for (int i = 0; i< range; i++)
+            {
+                if(!hashInt.Contains(i))
+                    return DecToString(i, nums.Length);
+            }
+
+            return "";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.DecToString(12,5));
+            Console.WriteLine(solution.StringToDex("11111"));
+            Console.ReadKey();
         }
     }
 }
