12d8499d19a89cb137d8155a09f12c1533efd5d6
Kumasen69 <kumasen69@gmail.com>
2023-11-22 14:03:13 +0900

    Leetcode1424 のベースコード

---
+++ medium/Leetcode1424_DTII/Leetcode1424_DTII/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode1424_DTII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
